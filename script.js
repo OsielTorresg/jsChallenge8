6a24dbeb79cd66c1e6d686d700bb417edbc56893
osiel torres gonzalez <osieltorresgonzalez@R2Hs-MacBook-Pro.local>
2022-01-09 22:36:35 -0500

    upload

---
+++ script.js
@@ -0,0 +1,69 @@
+//very easy challenge
+var num1 = 20;
+var num2 = 15;
+
+console.log(`the difference between ${num1} and ${num2} is ${num1 - num2}`);
+
+//easy challenge
+var name1 = "osiel";
+var name2 = "Srikanth";
+
+console.log(
+  `the name ${name1} is shorter then ${name2} by ${
+    name2.length - name1.length
+  } characters.`
+);
+// medium challenge
+
+let userTxt = prompt("Please insert a word");
+
+if (userTxt === userTxt.toUpperCase()) {
+  console.log(`why are you shouting?`);
+} else if (userTxt === userTxt.toLowerCase()) {
+  console.log(`why are you whispering?`);
+} else {
+  console.log(`what are you saying?`);
+}
+////hard
+//add
+function addition(x, y) {
+  console.log(x + y);
+}
+addition(1, 2);
+//subtract
+function subtract(x, y) {
+  console.log(x - y);
+}
+subtract(3, 4);
+//multi
+
+function multiplication(x, y) {
+  console.log(x * y);
+}
+multiplication(5, 6);
+///division
+function division(x, y) {
+  console.log(x / y);
+}
+division(4, 2);
+///// very hard
+/// get person to insert a number
+let firstNum = prompt(" insert a number please");
+/// options to choose examples
+let operator = prompt(" choose operation , (+ , -, *, /) ");
+
+//// choose second
+let secondNum = prompt("insert second number");
+let result;
+if (operator === `+`) {
+  result = firstNum + secondNum;
+} else if (operator === `-`) {
+  result = firstNum - secondNum;
+} else if (operator === `*`) {
+  result = firstNum * secondNum;
+} else {
+  result = firstNum / secondNum;
+}
+///result
+
+console.log(`${firstNum}  ${operator}  ${secondNum} = ${result}`);
